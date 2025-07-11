c1606917a2daa8c1c7742be3351ea2d32348a89c
Khaled <khaled.bozomqita@gmail.com>
2025-07-11 16:29:26 +0200

    Added ConnectionsString to the database

---
+++ Shop_Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop_Store.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
